5a9d2de97cbcd5bbdc8517d3b66e74dfc2ee903c
moisesdj@uninorte.edu.co <moisesdj@uninorte.edu.co>
2026-09-12 17:40:17 -0500

    codigos

---
+++ Assets/Assets/codigos/jugadores/jugadores.cs
@@ -0,0 +1,22 @@
+class Jugador
+{
+    public int vida,Id;
+    public string nombre;
+
+
+    public Jugador (int vida, int Id,string nombre){
+        this.vida=vida;
+        this.Id=Id;
+        this.nombre=nombre;
+    }
+}
+
+class torres
+{
+    public int daño;
+
+    public torres (int daño)
+    {
+        this.daño=daño;
+    }
+}
